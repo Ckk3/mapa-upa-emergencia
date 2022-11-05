69f10eb86d39de50da8dd0cb6269d66cf14a3c23
Filipe Lopes <filiperocklopes@gmail.com>
2022-11-05 07:26:56 -0300

    feat: add fillPdfAihSus in mutations

--- flaskapp/app/graphql/__init__.py
+++ flaskapp/app/graphql/__init__.py
@@ -6,20 +6,77 @@ from ariadne import gql, QueryType, load_schema_from_path
 # Define type definitions (schema) using SDL
 type_defs = gql(
     """
-   type Query {
+    type Query {
        places: [Place]
        "Resolver para servir de fixture para models de outra aplicação Django. `model` é a string que deve aparecer no campo model, `table` no momento deve ser um dos: `[ciap, cid10 , procedimento, vias, exame, medicamento]`. Atenção, o campo `fields` está como **String**"
        fixtures(model:String!, table:String!): [Fixture!]
        prescriptions: [String]
        records(cns:String): [MedicalRecord]
-   }
+    }
+
+    type Mutation {
+        "Criação de documento de AIH"
+        fillPdfAihSus(
+            establishmentSolitcName: String, 
+            establishmentSolitcCnes: Int, 
+            establishmentExecName: String, 
+            establishmentExecCnes: Int, 
+            patientName: String, 
+            patientCns: Int, 
+            patientBirthday: String, 
+            "Quais as opções?"
+            patientSex: String,             
+            patientMotherName: String, 
+            patientAdress: String, 
+            patientAdressCity: String, 
+            patientAdressCityIbgeCode: Int, 
+            patientAdressUF: String, 
+            patientAdressCEP: Int, 
+            mainClinicalSignsSymptoms: String, conditionsJustifyHospitalization: String, 
+            initialDiagnostic: String, 
+            principalCid10: String, 
+            "Não seria solicited procedure?"
+            procedureSolicited: String, 
+            procedureCode: String, 
+            clinic: String, 
+            internationCarater: String, 
+            "Em graphql não tem entrada dict, como seria?"
+            profSolicitorDocument: String, 
+            profSolicitorName: String, 
+            solicitationDatetime: String, 
+            autorizationProfName: String, 
+            emissionOrgCode: String, 
+            "Em graphql não tem entrada dict, como seria?"
+            autorizatonProfDocument: String, 
+            autorizatonDatetime: String, hospitalizationAutorizationNumber: Int,
+            examResults: String, 
+            chartNumber: Int, 
+            patientEthnicity: String, 
+            patientResponsibleName: String, 
+            "Não seria melhor o telefone ser tipo string?"
+            patientMotherPhonenumber: Int, 
+            patientResponsiblePhonenumber: Int, 
+            secondaryCid10: String, 
+            cid10AssociatedCauses: String, 
+            acidentType: String, 
+            insuranceCompanyCnpj: Int, 
+            insuranceCompanyTicketNumber: Int, 
+            insuranceCompanySeries: String,
+            companyCnpj: Int, 
+            companyCnae:int=None, 
+            company_cbor:int=None, 
+            pension_status:str=None
+        ){
+            base64Pdf: String
+        }
+    }
 
 
-   type Place {
+    type Place {
        name: String!
        description: String!
        country: String!
-       }  
+    }  
 
     type Fixture {
         pk: String
